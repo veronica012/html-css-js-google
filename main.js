238d0da343db1d39c744d62e58bf6b9db40d469e
Veronica Celseti <veronicacelseti12@gmail.com>
2020-05-06 12:33:30 +0200

    hide click 'ricordamelo più tardi'

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
     //intercettare il click sullicona del menu
     $('.fa-th').click(function() {
         var menu_visibile = $('.dropdown').toggle();
-    })
-
-
-
+    });
+    $('.privacy-bar-right a:first-child').click(function() {
+        var privacy_bar_hidden = $('.privacy-bar').hide();
+    });
 });
